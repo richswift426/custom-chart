b5e28eb102a4bcc2677033870c5c39d75cf14d8b
Hugo <hcubriac66@gmail.com>
2023-05-31 09:19:27 -0400

    replace canvas chart with appex chart

--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -1,101 +1,78 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Chart from 'chart.js/auto';
-import 'chartjs-plugin-zoom';
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
 import historyService from "../services/chartApi";
 import LoadingSpinner from "./LoadingSpinner";
 
-const MyChart = () => {
-  let chartInstance = null;
-
+const ZoomableChart = () => {
   const [dates, setDates] = useState([]);
   const [history, setHistory] = useState([]);
   const [history1, setHistory1] = useState([]);
   const [history2, setHistory2] = useState([]);
   const [spinner, setSpinner] = useState(false);
-  
-  const chartRef = useRef(null);
 
-  useEffect(() => {
-    if (chartRef.current) {
-      chartInstance = new Chart(chartRef.current, {
-        type: 'line',
-        data: {
-          labels: dates,
-          datasets: [
-            {
-              label: 'Traffic',
-              data: history
-            },
-            {
-              label: 'Traffic1',
-              data: history1
-            },
-            {
-              label: 'Traffic2',
-              data: history2
-            }
-          ],
-        },
-        options: {
-          plugins: {
-            title: {
-              display: true,
-              text: 'Hello'
-            },
-            zoomEnabled: true,
-            zoom: {
-              pan: {
-                enabled: true,
-                mode: 'xy',
-              },
-              zoom: {
-                wheel: {
-                  enabled: true,
-                },
-                pinch: {
-                  enabled: true,
-                },
-                mode: 'xy',
-              },
-            },
-          },
-          elements: {
-            line: {
-              tension: 0.4,
-              cubicInterpolationMode: 'monotone',
-            },
-          },
-        },
-      });
-    }
-
-    return () => {
-      if (chartInstance) {
-        chartInstance.destroy();
+  const [options, setOptions] = useState({
+    chart: {
+      id: "zoomable-chart",
+      zoom: {
+        enabled: true,
+        type: "xy"
       }
-    };
-  }, [chartRef, dates, history, history1, history2]);
+    },
+    xaxis: {
+      categories: []
+    }
+  });
+
+  const [series, setSeries] = useState([
+    {
+      name: "Sales",
+      data: []
+    },
+    {
+      name: "Expenses",
+      data: []
+    }
+  ]);
 
   const getHistoryData = async () => {
     await historyService.getHistoricalData()
     .then(
       result => {
         console.log('result: ', result);
-        setDates(result.data.data[0]);
-        setHistory(result.data.data[1]);
-        setHistory1(result.data.data[2]);
-        setHistory2(result.data.data[3]);
+        // setDates(result.data.data[0]);
+        // setHistory(result.data.data[1]);
+        // setHistory1(result.data.data[2]);
+        // setHistory2(result.data.data[3]);
+        setOptions({
+          chart: {
+            id: "zoomable-chart",
+            zoom: {
+              enabled: true,
+              type: "xy"
+            }
+          },
+          xaxis: {
+            categories: result.data.data[0]
+          }
+        });
+        setSeries([
+          {
+            name: "History Data1",
+            data: result.data.data[1]
+          },
+          {
+            name: "History Data2",
+            data: result.data.data[2]
+          },
+          {
+            name: "History Data3",
+            data: result.data.data[3]
+          }
+        ]);
         setSpinner(false);
       }
     )
   }
-
-  const handleWheel = (event) => {
-    event.preventDefault();
-    if (chartInstance) {
-      chartInstance.zoom(event.deltaY > 0 ? -1 : 1);
-    }
-  };
 
   const handleClick = () => {
     setSpinner(true);
@@ -103,8 +80,8 @@ const MyChart = () => {
   }
 
   return (
-    <div className="mx-auto w-3/5 overflow-hidden">
-      <canvas ref={chartRef} />
+    <div className="chart">
+      <Chart options={options} series={series} type="line" height={350} />
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-5 w-28 flex justify-center items-center" onClick={()=>handleClick()}>
         {spinner ? <LoadingSpinner /> : "Load Data"}
       </button>
@@ -112,4 +89,4 @@ const MyChart = () => {
   );
 };
 
-export default MyChart;
+export default ZoomableChart;
