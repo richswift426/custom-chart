2dbd050ecafeb6ea0e5c1191f1972fb160dc0055
Hugo <hcubriac66@gmail.com>
2023-05-31 11:45:30 -0400

    add banner

--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -166,7 +166,7 @@ const ZoomableChart = () => {
           </button>
         </div>
       </div>
-      <Chart options={options} series={series} type="line" height={350} className="mt-8" />
+      <Chart options={options} series={series} type="line" height={350} className="mt-8 w-vw" />
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-5 w-28 flex justify-center items-center" onClick={()=>handleClick()}>
         {spinner ? <LoadingSpinner /> : "Load Data"}
       </button>
